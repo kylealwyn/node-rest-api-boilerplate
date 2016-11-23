2664e7c6cf2d7a55e4c91895ed0e362d98c673d0
Kyle Alwyn <kjalwyn@gmail.com>
2016-11-23 12:01:50 -0800

    add access control

---
+++ app/middleware/access-control.js
@@ -0,0 +1,13 @@
+import authenticate from './authenticate';
+
+export default function accessControl(role) {
+  if (!role) {
+    throw new Error('Required role needs to be set');
+  }
+
+  return (req, res, next) => {
+    authenticate(req, res, () => {
+      next()
+    })
+  }
+}
