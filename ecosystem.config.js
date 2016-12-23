ce26c497c4d86679735b8a1b9219babdbdc7f143
Kyle Alwyn <kjalwyn@gmail.com>
2016-12-23 11:48:08 -0800

    add pm2 deployment config

---
+++ ecosystem.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  apps: [
+    {
+      name: 'API',
+      script: 'build/server.js',
+      env: {
+        COMMON_VARIABLE: 'true',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    dev: {
+      'user': 'deploy',
+      'host': '54.149.99.50',
+      'ref': 'origin/deploy',
+      'repo': 'git@github.com:kylealwyn/node-rest-api-boilerplate.git',
+      'path': '/var/www/node-boilerplate',
+      'post-setup': 'npm install --global pm2',
+      'post-deploy': 'npm run package && npm run serve:dev',
+      'env': {
+        NODE_ENV: 'development',
+      },
+    },
+    production: {
+      'user': 'node',
+      'host': '212.83.163.1',
+      'ref': 'origin/master',
+      'repo': 'git@github.com:repo.git',
+      'path': '/var/www/production',
+      'post-deploy': 'npm install --productoin && ',
+    },
+  },
+};
